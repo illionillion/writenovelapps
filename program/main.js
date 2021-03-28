6f6a25a2e417de66915797c514eb2f767c1decee
illionillion <yuto.0526.sano@gmail.com>
2021-03-28 20:30:57 +0900

    頑張ってマス目にしようと思ったけどなんかムズイ

--- program/main.js
+++ program/main.js
@@ -1,1 +1,12 @@
 console.log('OK!!');
+
+var area= document.getElementById('text');
+var number=document.getElementById('number');
+
+area.oninput=function write(){
+  var text=area.innerText;
+  console.log(text);
+  var num =text.length;
+  number.innerHTML=num;
+
+}
